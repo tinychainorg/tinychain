a3ca6df11d89bba8f081c04de8a6b459c8f5b40a
liamzebedee <liamzebedee@yahoo.com.au>
2023-12-02 22:23:47 +1100

    add tx structure

--- transaction.py
+++ transaction.py
@@ -1,12 +1,23 @@
 from hashlib import sha256
 import yaml
+from wallet import Wallet
 
-def parse_text_tx(txt):
+def decode_tx_yaml(txt):
     data = yaml.safe_load(txt)
     return Tx(data['from'], data['to'], data['data'], data['sig'])
 
+def encode_tx_yaml(tx):
+    data = {
+        'from': tx.from_acc,
+        'to': tx.to_acc,
+        'data': tx.data.hex(),
+        'sig': tx.sig.hex()
+    }
+    # Dump keys in order defined above.
+    return yaml.dump(data, default_flow_style=False, sort_keys=False)
+
 class Tx:
-    def __init__(self, from_acc, to_acc, data, sig):
+    def __init__(self, from_acc=None, to_acc=None, data=None, sig=None):
         self.from_acc = from_acc
         self.to_acc = to_acc
         self.data = data
@@ -15,6 +26,33 @@ class Tx:
     def parse(self, str):
         pass
 
+    def __repr__(self) -> str:
+        sig_str = "" if self.sig is None else self.sig.hex()
+        return "Tx(from={}, to={}, data={}, sig={})".format(self.from_acc, self.to_acc, self.data, sig_str)
+
+    # The envelope is the data we are signing.
+    # It excludes the signature.
+    def envelope(self):
+        return b"".join([
+            bytes.fromhex(self.from_acc),
+            bytes.fromhex(self.to_acc),
+            self.data
+        ])
+
 
 if __name__ == "__main__":
-    print(parse_text_tx(open("testnet-1/txs/1.yaml").read()))
+    wallet_liam = Wallet.create_random()
+    wallet_sylve = Wallet.create_random()
+
+    tx = Tx()
+    tx.from_acc = wallet_liam.pubkey_str()
+    tx.to_acc = wallet_sylve.pubkey_str()
+    tx.data = b"hello sylve"
+    tx.sig = wallet_liam.sign(tx.envelope())
+
+    print("tx: {}".format(tx))
+
+    is_valid = Wallet.verify(tx.from_acc, tx.sig, tx.envelope())
+    print("Verify signature: {}".format(is_valid))
+
+    print(encode_tx_yaml(tx))
