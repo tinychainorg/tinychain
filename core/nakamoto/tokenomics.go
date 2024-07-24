e13adfcdfcbfa8d4c56f91a6b17c83b396499693
liamzebedee <liamzebedee@yahoo.com.au>
2024-07-24 15:09:22 +1000

    add a tokenomics / supply curve module, visualise

---
+++ core/nakamoto/tokenomics.go
@@ -0,0 +1,18 @@
+package nakamoto
+
+import (
+	"math"
+)
+
+// GetBlockReward returns the block reward for a given block height.
+func GetBlockReward(blockHeight int) float64 {
+	initialReward := 50.0
+	halvingInterval := 210000
+
+	// Calculate the number of halvings
+	numHalvings := blockHeight / halvingInterval
+
+	// Calculate the reward after the halvings
+	reward := initialReward / math.Pow(2, float64(numHalvings))
+	return reward
+}
